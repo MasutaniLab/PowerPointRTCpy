34b5c83b62a98d3b068f3ab0a3eec64706879b80
Nobu19800 <TyouKyozyakuTaisitu@yahoo.co.jp>
2014-12-10 21:26:12 +0900

    2126

--- PowerPointRTC.py
+++ PowerPointRTC.py
@@ -5,14 +5,6 @@
 #   @file PowerPointRTC.py
 #   @brief PortPointControl Component
 
-import win32com
-import pythoncom
-import pdb
-from win32com.client import *
-import pprint
-import datetime
-import msvcrt
-
 
 
 
@@ -35,9 +27,8 @@ from OpenRTM_aist import CorbaConsumer
 from omniORB import CORBA
 import CosNaming
 
-import ctypes
-
 from ImpressControl import *
+from PowerPointObject import *
 
 
 powerpointcontrol_spec = ["implementation_id", "PowerPointControl",
@@ -187,10 +178,6 @@ class PowerPointControl(ImpressControl):
     self.m_powerpoint.run()
     self.m_powerpoint.gotoSlide(self.SlideFileInitialNumber[0])
     self.slidenum = self.SlideFileInitialNumber[0]
-    #print self.m_powerpoint.ptPresentation.Slides(1).CustomLayout.Height
-    #print self.m_powerpoint.ptPresentation.Slides(1).CustomLayout.Width
-    #print self.m_powerpoint.ptSlideShowWindow.Height
-    #print self.m_powerpoint.ptSlideShowWindow.Width
     
     return RTC.RTC_OK
 
@@ -209,7 +196,7 @@ class PowerPointControl(ImpressControl):
   
   def onExecute(self, ec_id):
     ImpressControl.onExecute(self, ec_id)
-        
+    
 
     return RTC.RTC_OK
 
@@ -221,7 +208,7 @@ class PowerPointControl(ImpressControl):
   # @return RTC::ReturnCode_t
   
   def on_shutdown(self, ec_id):
-      ImpressControl.on_shutdown(self, ec_id)
+      ImpressControl.onExecute(self, ec_id)
       return RTC.RTC_OK
 
 
@@ -231,206 +218,7 @@ class PowerPointControl(ImpressControl):
       
 
   
-##
-# @class PowerPointObject
-# @brief PowerPointを操作するクラス
-#
-class PowerPointObject:
-    ppLayoutBlank = 12
-    ppLayoutChart = 8
-    ppLayoutChartAndText = 6
-    ppLayoutClipartAndText = 10
-    ppLayoutClipArtAndVerticalText = 26
-    ppLayoutCustom = 32
-    ppLayoutFourObjects = 24
-    ppLayoutLargeObject = 15
-    ppLayoutMediaClipAndText = 18
-    ppLayoutMixed = -2
-    ppLayoutObject = 16
-    ppLayoutObjectAndText = 14
-    ppLayoutObjectAndTwoObjects = 30
-    ppLayoutObjectOverText = 19
-    ppLayoutOrgchart = 7
-    ppLayoutTable = 4
-    ppLayoutText = 2
-    ppLayoutTextAndChart = 5
-    ppLayoutTextAndClipart = 9
-    ppLayoutTextAndMediaClip = 17
-    ppLayoutTextAndObject = 13
-    ppLayoutTextAndTwoObjects = 21
-    ppLayoutTextOverObject = 20
-    ppLayoutTitle = 1
-    ppLayoutTitleOnly = 11
-    ppLayoutTwoColumnText = 3
-    ppLayoutTwoObjects = 29
-    ppLayoutTwoObjectsAndObject = 31
-    ppLayoutTwoObjectsAndText = 22
-    ppLayoutTwoObjectsOverText = 23
-    ppLayoutVerticalText = 25
-    ppLayoutVerticalTitleAndText = 27
-    ppLayoutVerticalTitleAndTextOverChart = 28
 
-    ##
-    # @brief コンストラクタ
-    # @param self 
-    #
-    def __init__(self):
-        self.filename = " "
-        
-        self.ptApplication = None
-        self.ptPresentations = None
-        self.ptPresentation = None
-        self.ptSlideShowWindow = None
-        self.ptSlideShowView = None
-        
-
-        self.thread_ptApplication = None
-        self.thread_ptPresentations = None
-        self.thread_ptPresentation = None
-
-        self.t_ptApplication = None
-        self.t_ptPresentations = None
-        self.t_ptPresentation = None
-
-    ##
-    # @brief 
-    # @param self
-    #
-    def run(self):
-      self.ptSlideShowWindow = self.ptPresentation.SlideShowSettings.Run()
-      self.ptSlideShowView = self.ptSlideShowWindow.View
-      
-    ##
-    # @brief 
-    # @param self
-    #
-    def end(self):
-      self.ptSlideShowView.Exit()
-      self.ptSlideShowWindow = None
-      self.ptSlideShowView = None
-      
-    ##
-    # @brief 
-    # @param self
-    #
-    def gotoSlide(self, num):
-      
-      if 0 < num and num <=  self.ptPresentation.Slides.Count:
-        self.ptSlideShowView.GotoSlide(num)
-        return True
-      else:
-        return False
-
-
-        
-    ##
-    # @brief 
-    # @param self 
-    #
-    def next(self):
-      self.ptSlideShowView.Next()
-    ##
-    # @brief 
-    # @param self 
-    #
-    def previous(self):
-      self.ptSlideShowView.Previous()
-    ##
-    # @brief 
-    # @param self
-    # @param bx
-    # @param by
-    # @param ex
-    # @param ey 
-    #
-    def drawLine(self, bx, by, ex, ey):
-      tmp = float(self.ptPresentation.Slides(1).CustomLayout.Height)/float(ctypes.windll.user32.GetSystemMetrics(1))
-      
-      #print self.ptPresentation.Slides(1).CustomLayout.Height
-      #print self.ptPresentation.Slides(1).CustomLayout.Width
-      
-      self.ptSlideShowView.DrawLine(bx*tmp, by*tmp, ex*tmp, ey*tmp)
-
-    ##
-    # @brief 
-    # @param self 
-    #
-    def eraseDrawing(self):
-      self.ptSlideShowView.EraseDrawing()
-    
-    ##
-    # @brief 
-    # @param self 
-    #
-    def preInitCom(self):
-        self.thread_ptApplication = pythoncom.CoMarshalInterThreadInterfaceInStream (pythoncom.IID_IDispatch, self.t_ptApplication)
-        self.thread_ptPresentations = pythoncom.CoMarshalInterThreadInterfaceInStream (pythoncom.IID_IDispatch, self.t_ptPresentations)
-        self.thread_ptPresentation = pythoncom.CoMarshalInterThreadInterfaceInStream (pythoncom.IID_IDispatch, self.t_ptPresentation)
-
-    ##
-    # @brief 
-    # @param self 
-    #
-    def initCom(self):
-        if self.ptApplication == None:
-          pythoncom.CoInitialize()
-          self.ptApplication = win32com.client.Dispatch ( pythoncom.CoGetInterfaceAndReleaseStream (self.thread_ptApplication, pythoncom.IID_IDispatch))
-          self.ptPresentations = win32com.client.Dispatch ( pythoncom.CoGetInterfaceAndReleaseStream (self.thread_ptPresentations, pythoncom.IID_IDispatch))
-          self.ptPresentation = win32com.client.Dispatch ( pythoncom.CoGetInterfaceAndReleaseStream (self.thread_ptPresentation, pythoncom.IID_IDispatch))
-          
-    ##
-    # @brief 
-    # @param self 
-    #
-    def closeCom(self):
-        pythoncom.CoUninitialize()
-
-    ##
-    # @brief PowerPointファイルを開く関数
-    # @param self 
-    # @param fn ファイルパス
-    #
-    def Open(self, fn):
-        if self.filename == fn:
-            return
-        self.filename = fn
-
-        
-
-        try:
-            if self.ptApplication == None:
-              t_ptApplication = win32com.client.Dispatch("PowerPoint.Application")
-            else:
-              t_ptApplication = self.ptApplication
-            
-            
-            t_ptApplication.Visible = True
-            try:
-                t_ptPresentations = t_ptApplication.Presentations
-                
-
-                try:
-                    t_ptPresentation = None
-                    if self.filename == "":
-                        t_ptPresentation = t_ptPresentations.Add()
-                        t_ptPresentation.Slides.Add(1,PowerPointObject.ppLayoutTitleOnly)
-                    else:
-                        t_ptPresentation = t_ptPresentations.Open(self.filename)
-
-                    
-                    self.t_ptApplication = t_ptApplication
-                    self.t_ptPresentations = t_ptPresentations
-                    self.t_ptPresentation = t_ptPresentation
-
-                    self.preInitCom()
-
-                    
-                except:
-                    return
-            except:
-                return
-        except:
-            return
 
 
 ##
